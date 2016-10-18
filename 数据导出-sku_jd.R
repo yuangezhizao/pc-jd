c74c7dc87cddf59d846dfc49a7781b23a13da2d4
zhuanglineu <444856410@qq.com>
2016-10-18 16:52:09 +0800

    new

--- 数据导出-sku_jd.R
+++ 数据导出-sku_jd.R
@@ -1,5 +1,5 @@
 #################
-#导出customer.sku_jd
+#导出数据库中的表
 #按抓取编号导出,每次导出一个抓取编号
 
 #################
@@ -8,9 +8,12 @@ rm(list=ls(all=T));gc()
 library(data.table)
 library(RODBC)
 
-crawl_id <- scan()
-mysql <- odbcConnect('customer',uid='root',pwd='1111')
-sql <- paste('select * from sku_jd where crawl_id=', crawl_id, sep='')
-sku_jd <- data.table(sqlQuery(mysql, sql))
-path <- paste('f:/customer/', 'sku_jd_', crawl_id, '.csv', sep='')
-write.csv(sku_jd, path, row.names=F)
+output <- function(crawl_id, table_name)
+{
+mysql <- odbcConnect('customer', uid='root', pwd='1111', DBMSencoding='utf8')
+sql <- paste('select * from ', table_name, ' where crawl_id=', crawl_id, sep='')
+data <- data.table(sqlQuery(mysql, sql))
+path <- paste('f:/customer/', table_name, '_', crawl_id, '.csv', sep='')
+write.csv(data, path, row.names=F)
+odbcCloseAll()
+}
