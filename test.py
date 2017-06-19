2a433da03e22dc79f3994c527dd1c570174b0ba4
zhuanglineu <444856410@qq.com>
2017-06-19 12:49:34 +0800

    new

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 28 17:43:17 2017
+
+@author: Administrator
+"""
+for i in range(0, 10):
+    try:
+        r = 1 / (i - 5)
+        print(r)
+    except:
+        runfile('C:/Users/Administrator/Documents/GitHub/pc-jd/test.py', 
+                wdir='C:/Users/Administrator/Documents/GitHub/pc-jd')
+        
